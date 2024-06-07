6c5023d84dc08bbc4389d7ef94eb062d3ee21f3a
GalinaChechkina <galina.chechkina@gmail.com>
2024-06-07 10:48:42 +0200

    new_footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,17 +1,27 @@
-import React from "react"
-import { Link } from "react-router-dom"
-import styles from "./footer.module.css"
+import React, { useEffect, useState } from "react";
+import styles from "./footer.module.css";
+
 export default function Footer() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 0);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <div className={styles.footerContainer}>
-      <p>© All rights reserved</p>
-      <p>© 2024 Team Students of the AIT TR School</p>
-      <p>© Volunteer site for helping animals "Take me home"</p>
- 
-      <a href="https://www.ait-tr.de/en" target="blank" className={styles.a_footer}>© www.ait-tr.de</a>
-     
-
-
+    <div className={`${styles.footerContainer} ${isScrolled ? styles.scrolled : ""}`}>
+      <div className={styles.footerContent}>
+        <p>© All rights reserved</p>
+        <p>© 2024 Team Students of the AIT TR School</p>
+        <p>© Volunteer site for helping animals "Take me home"</p>
+        <a href="https://www.ait-tr.de/en" target="_blank" className={styles.aFooter}>© www.ait-tr.de</a>
+      </div>
     </div>
-  )
+  );
 }
